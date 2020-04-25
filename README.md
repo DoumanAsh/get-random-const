9a9d0f24dc76f46ec41357151afb558c2440e3fe
Douman <douman@gmx.se>
2020-04-25 10:55:55 +0200

    Finalize

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # get-random-const
 
-WIP macro to generate random at compile time
+![](https://github.com/DoumanAsh/get-random-const/workflows/Rust/badge.svg)
+[![Crates.io](https://img.shields.io/crates/v/get-random-const.svg)](https://crates.io/crates/get-random-const)
+[![Documentation](https://docs.rs/get-random-const/badge.svg)](https://docs.rs/crate/get-random-const/)
+
+Macro to generate random at compile time
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,35 @@
 //! ## Example
 //!
 //! ```rust
-//! #![feature(proc_macro_hygiene)]
 //! use get_random_const::random;
+//!
+//! #[random]
+//! pub const RAND: i32 = 0;
+//!
+//! {
+//! #[random]
+//! static STATIC_ARRAY: [u8; 5] = [];
+//! #[random]
+//! static STATIC_ARRAY_SIGN: [i8; 5] = [];
+//! }
+//!
+//! {
+//! #[random]
+//! static STATIC_ARRAY: [u16; 10] = [];
+//! }
+//! {
+//! #[random]
+//! static STATIC_ARRAY: [i16; 10] = [];
+//! }
+//!
+//! {
+//! #[random]
+//! static STATIC_ARRAY: [u64; 12] = [];
+//! }
+//! {
+//! #[random]
+//! static STATIC_ARRAY: [i64; 12] = [];
+//! }
 //!
 //! #[random]
 //! static STATIC_LOLKA: i128 = 0;
@@ -15,15 +42,13 @@
 //! assert_ne!(LOLKA, 0);
 //!
 //! #[random]
-//! let lolka: u8;
-//! assert_ne!(lolka, 0);
+//! const LOLKA_ARRAY: [u32; 32] = 0;
 //!
-//! println!("lolka={}", lolka);
-//!
+//! //Requires #![feature(proc_macro_hygiene)]
 //! //#[random]
-//! //let lolka_arr: [u16; 14];
+//! //let lolka: u8;
+//! //assert_ne!(lolka, 0);
 //!
-//! //println!("lolka_arr={:?}", lolka_arr);
 //! ```
 
 extern crate proc_macro;
@@ -32,62 +57,112 @@ use quote::quote;
 use proc_macro::TokenStream;
 use getrandom::getrandom;
 
-fn randomize<T: Copy>(val: &mut T) {
+fn randomize<T: Copy>() -> T {
+    let mut val = core::mem::MaybeUninit::<T>::uninit();
     let slice = unsafe {
-        core::slice::from_raw_parts_mut(val as *mut _ as *mut u8, core::mem::size_of::<T>())
+        core::slice::from_raw_parts_mut(val.as_mut_ptr() as *mut u8, core::mem::size_of::<T>())
     };
 
     getrandom(slice).expect("To generate random");
+    unsafe {
+        val.assume_init()
+    }
+}
+
+fn map_array_init(path: &syn::TypeArray) -> Option<proc_macro2::TokenStream> {
+    let ty = match &*path.elem {
+        &syn::Type::Path(ref path) => &path.path,
+        _ => return None,
+    };
+
+    let gen: Box<dyn Fn() -> String> = if ty.is_ident("u8"){
+        Box::new(|| format!("{}u8", randomize::<u8>()))
+    } else if ty.is_ident("i8") {
+        Box::new(|| format!("{}i8", randomize::<i8>()))
+    } else if ty.is_ident("u16") {
+        Box::new(|| format!("{}u16", randomize::<u16>()))
+    } else if ty.is_ident("i16") {
+        Box::new(|| format!("{}i16", randomize::<i16>()))
+    } else if ty.is_ident("u32") {
+        Box::new(|| format!("{}u32", randomize::<u32>()))
+    } else if ty.is_ident("i32") {
+        Box::new(|| format!("{}i32", randomize::<i32>()))
+    } else if ty.is_ident("u64") {
+        Box::new(|| format!("{}u64", randomize::<u64>()))
+    } else if ty.is_ident("i64") {
+        Box::new(|| format!("{}i64", randomize::<i64>()))
+    } else if ty.is_ident("u128") {
+        Box::new(|| format!("{}u128", randomize::<u128>()))
+    } else if ty.is_ident("i128") {
+        Box::new(|| format!("{}i128", randomize::<i128>()))
+    } else if ty.is_ident("usize") {
+        Box::new(|| format!("{}usize", randomize::<usize>()))
+    } else if ty.is_ident("isize") {
+        Box::new(|| format!("{}isize", randomize::<isize>()))
+    } else {
+        return None
+    };
+
+    let len = match &path.len {
+        &syn::Expr::Lit(ref literal) => match &literal.lit {
+            &syn::Lit::Int(ref int) => match int.base10_parse::<usize>() {
+                Ok(result) => result,
+                Err(_) => return None,
+            },
+            _ => return None,
+        },
+        _ => return None,
+    };
+
+    let mut init = "[".to_owned();
+
+    for _ in 0..len {
+        init.push_str(gen().as_str());
+        init.push(',');
+    }
+
+    init.pop();
+    init.push(']');
+
+    init.parse().ok()
 }
 
 fn map_path_to_init_expr(path: &syn::Path) -> Option<proc_macro2::TokenStream> {
     let res = if path.is_ident("u8"){
-        let mut res = 0u8;
-        randomize(&mut res);
+        let res = randomize::<u8>();
         quote! { #res }
     } else if path.is_ident("i8") {
-        let mut res = 0i8;
-        randomize(&mut res);
+        let res = randomize::<i8>();
         quote! { #res }
     } else if path.is_ident("u16") {
-        let mut res = 0i16;
-        randomize(&mut res);
+        let res = randomize::<u16>();
         quote! { #res }
     } else if path.is_ident("i16") {
-        let mut res = 0i16;
-        randomize(&mut res);
+        let res = randomize::<i16>();
         quote! { #res }
     } else if path.is_ident("u32") {
-        let mut res = 0u32;
-        randomize(&mut res);
+        let res = randomize::<u32>();
         quote! { #res }
     } else if path.is_ident("i32") {
-        let mut res = 0i32;
-        randomize(&mut res);
+        let res = randomize::<i32>();
         quote! { #res }
     } else if path.is_ident("u64") {
-        let mut res = 0u64;
-        randomize(&mut res);
+        let res = randomize::<u64>();
         quote! { #res }
     } else if path.is_ident("i64") {
-        let mut res = 0i64;
-        randomize(&mut res);
+        let res = randomize::<i64>();
         quote! { #res }
     } else if path.is_ident("u128") {
-        let mut res = 0u128;
-        randomize(&mut res);
+        let res = randomize::<u128>();
         quote! { #res }
     } else if path.is_ident("i128") {
-        let mut res = 0i128;
-        randomize(&mut res);
+        let res = randomize::<i128>();
         quote! { #res }
     } else if path.is_ident("usize") {
-        let mut res = 0usize;
-        randomize(&mut res);
+        let res = randomize::<usize>();
         quote! { #res }
     } else if path.is_ident("isize") {
-        let mut res = 0isize;
-        randomize(&mut res);
+        let res = randomize::<isize>();
         quote! { #res }
     } else {
         return None
@@ -104,13 +179,13 @@ pub fn random(_args: TokenStream, input: TokenStream) -> TokenStream {
         syn::Stmt::Local(ref mut local) => {
             let init = match &local.pat {
                 syn::Pat::Type(ref typ) => match &*typ.ty {
-                    //syn::Type::Array(ref arr) => {
-                    //    println!("arr={:?}", arr);
-                    //    unreachable!()
-                    //},
                     syn::Type::Path(ref pt) => match map_path_to_init_expr(&pt.path) {
                         Some(init) => init,
                         None => return syn::Error::new_spanned(input, "Specified type is not simple integer").to_compile_error().into(),
+                    },
+                    syn::Type::Array(ref arr) => match map_array_init(arr) {
+                        Some(init) => init,
+                        None => return syn::Error::new_spanned(input, "Specified type is not integer array").to_compile_error().into(),
                     },
                     _ => return syn::Error::new_spanned(input, "Invalid type, must integer").to_compile_error().into(),
                 },
@@ -134,6 +209,10 @@ pub fn random(_args: TokenStream, input: TokenStream) -> TokenStream {
                         Some(init) => init,
                         None => return syn::Error::new_spanned(input, "Specified type is not simple integer").to_compile_error().into(),
                     },
+                    syn::Type::Array(ref arr) => match map_array_init(arr) {
+                        Some(init) => init,
+                        None => return syn::Error::new_spanned(input, "Specified type is not integer array").to_compile_error().into(),
+                    },
                     _ => return syn::Error::new_spanned(input, "Invalid type, must integer").to_compile_error().into(),
                 };
 
@@ -149,6 +228,10 @@ pub fn random(_args: TokenStream, input: TokenStream) -> TokenStream {
                     syn::Type::Path(ref pt) => match map_path_to_init_expr(&pt.path) {
                         Some(init) => init,
                         None => return syn::Error::new_spanned(input, "Specified type is not simple integer").to_compile_error().into(),
+                    },
+                    syn::Type::Array(ref arr) => match map_array_init(arr) {
+                        Some(init) => init,
+                        None => return syn::Error::new_spanned(input, "Specified type is not integer array").to_compile_error().into(),
                     },
                     _ => return syn::Error::new_spanned(input, "Invalid type, must integer").to_compile_error().into(),
                 };
